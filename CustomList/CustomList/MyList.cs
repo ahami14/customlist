9733bf138cf37056cebb8b7e6698200d9edf6272
ahami14 <ahnikkahamilton14@gmail.com>
2019-10-08 10:50:23 -0500

    initial upload of files

---
+++ CustomList/CustomList/MyList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class MyList<T>
+    {
+        private T[] items;
+
+        public MyList()
+        {
+            items = new T[4];
+        }
+
+        public void Add(T itemToAdd)
+        {
+
+        }
+    }
+}
